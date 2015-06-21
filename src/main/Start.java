b7e7ad3bb72aa4aee81109bc13b887b93114328e
robert <vjroby@gmail.com>
2015-06-21 21:28:45 +0300

    test path

--- src/main/Start.java
+++ src/main/Start.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("/test")
 public class Start {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
