83168aac8e6e2b99a0745a49f459ce429dd4dc69
robert <vjroby@gmail.com>
2015-06-21 22:17:34 +0300

    test first deploy

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,12 +1,12 @@
-package main;
+package main.java;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/test")
-public class Start {
+@Path("/")
+public class Home {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getHome(){
