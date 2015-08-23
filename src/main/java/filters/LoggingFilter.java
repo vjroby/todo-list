182b5a64fc51af70ef93d545fbd2956a5acf75b8
robert <vjroby@gmail.com>
2015-08-23 20:51:50 +0300

    added index.jsp file and change the path or jersey and filters

---
+++ src/main/java/filters/LoggingFilter.java
@@ -0,0 +1,29 @@
+package main.java.filters;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+public class LoggingFilter extends com.sun.jersey.api.container.filter.LoggingFilter implements ContainerResponseFilter{
+
+    private static final ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+
+    public static boolean vorboseLogging = false;
+
+    @Override
+    public ContainerRequest filter(ContainerRequest arg0) {
+        startTime.set(System.currentTimeMillis());
+        String ip = arg0.getHeaderValue("X-Forwarded-For");
+        System.out.println("IP reqest: "+ ip);
+        return arg0;
+    }
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response){
+        System.out.println(System.currentTimeMillis() - startTime.get());
+        StringBuilder sb = new StringBuilder();
+        sb.append("User:").append((request.getUserPrincipal() == null ? "unknown" : request.getUserPrincipal().getName()));
+        sb.append(" - Path:").append(request.getRequestUri().getPath());
+        System.out.println(sb);
+        return response;
+    }
+}
