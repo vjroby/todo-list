0fb14377b83088fc22610f03737e55efeba96438
robertdinu <robert.dinu@chinookbook.net>
2015-11-13 09:21:27 +0200

    enable session concurrency

--- src/main/java/ro/robertgabriel/configuration/SecurityBootstrap.java
+++ src/main/java/ro/robertgabriel/configuration/SecurityBootstrap.java
@@ -3,4 +3,9 @@ package ro.robertgabriel.configuration;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityBootstrap extends AbstractSecurityWebApplicationInitializer{
+
+    @Override
+    protected boolean enableHttpSessionEventPublisher() {
+        return  true;
+    }
 }
